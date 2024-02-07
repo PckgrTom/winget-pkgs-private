1f070e2b645f34fbd2c3e89c1ffd4d2ddbc56e04
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2024-02-07 12:43:13 +0530

    Merge APIs

--- api/packageManifests.go
+++ api/packageManifests.go
@@ -1,14 +1,22 @@
 package handler
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-	"net/url"
 	"os"
 	"slices"
+	"strings"
 
 	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+var (
+	WINGET_PKGS_OWNER     = os.Getenv("VERCEL_GIT_REPO_OWNER")
+	WINGET_PKGS_REPO_NAME = os.Getenv("VERCEL_GIT_REPO_SLUG")
+	WINGET_PKGS_BRANCH    = os.Getenv("VERCEL_GIT_COMMIT_REF")
 )
 
 func PackageManifests(w http.ResponseWriter, r *http.Request) {
@@ -30,21 +38,42 @@ func PackageManifests(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := []ApiResponse{}
-	res, err := http.Get(os.Getenv("API_URL") + "/manifests_github?package_identifier=" + url.QueryEscape(pkg_id))
-	// error will only be of type *url.Error, so added check for status code as well
-	if err != nil || res.StatusCode != http.StatusOK {
-		// we assume that the error is because the package was not found because
-		// the API seems to be stable 🙂 and the only error that can occur is when the package is not found
+	url := fmt.Sprintf("https://codeload.github.com/%s/%s/zip/refs/heads/%s", WINGET_PKGS_OWNER, WINGET_PKGS_REPO_NAME, WINGET_PKGS_BRANCH)
+	path := "/tmp/winget-pkgs.zip"
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := downloadRepository(url, path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "error downloading winget-pkgs repository: %s", err)
+			return
+		}
+	}
+
+	repoZip, err := zip.OpenReader(path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "error opening %s for reading: %s", path, err)
+		return
+	}
+
+	pkg_versions := getVersions(strings.ToLower(pkg_id), repoZip)
+	if len(pkg_versions) == 0 {
 		w.WriteHeader(http.StatusNoContent)
 		w.Write([]byte(fmt.Sprintf("package %s not found in repo", pkg_id)))
 		return
 	}
-	defer res.Body.Close()
-	json.NewDecoder(res.Body).Decode(&response)
+
+	result := []VersionAndManifests{}
+	for _, version := range pkg_versions {
+		manifests := getManifests(strings.ToLower(pkg_id), version, repoZip)
+		result = append(result, VersionAndManifests{
+			Version:   version,
+			Manifests: manifests,
+		})
+	}
 
 	result_data_versions := []Result_Data_Versions{}
-	for _, resp := range response {
+	for _, resp := range result {
 		var installers, default_locale interface{}
 		var locales []interface{}
 		for _, manifest_raw := range resp.Manifests {
@@ -135,12 +164,90 @@ type Result_Data_Versions struct {
 	Locales        []interface{}
 }
 
-type ApiResponse struct {
-	Version   string `json:"Version"`
-	Manifests []struct {
-		FileName string `json:"FileName"`
-		Content  string `json:"Content"`
-	} `json:"Manifests"`
+type VersionAndManifests struct {
+	Version   string
+	Manifests []Manifest
+}
+
+type Manifest struct {
+	FileName string
+	Content  string
+}
+
+func getVersions(pkg_id string, zipFile *zip.ReadCloser) []string {
+	pkg_path := getPackagePath(pkg_id, "")
+	versions := []string{}
+	for _, file := range zipFile.File {
+		file_name_lower := strings.ToLower(file.Name)
+		if !strings.HasPrefix(file_name_lower, pkg_path) || !file.Mode().IsDir() {
+			continue
+		}
+		if v := file.Name[len(pkg_path)+1:]; v != "" {
+			versions = append(versions, strings.TrimSuffix(v, "/"))
+		}
+	}
+	// remove sub-packages
+	for i := 0; i < len(versions); i++ {
+		is_sub_package := false
+		for j := 0; j < len(versions); j++ {
+			if strings.HasPrefix(versions[j], versions[i]) && versions[j] != versions[i] {
+				is_sub_package = true
+				versions = append(versions[:j], versions[j+1:]...)
+				j--
+			}
+		}
+		if is_sub_package {
+			versions = append(versions[:i], versions[i+1:]...)
+			i--
+		}
+	}
+	return versions
+}
+
+func getManifests(pkg_id, version string, zipFile *zip.ReadCloser) []Manifest {
+	pkg_path := getPackagePath(pkg_id, version)
+	version_manfiest_path := getPackagePath(pkg_id, version, fmt.Sprintf("%s.yaml", pkg_id))
+	manifests := []Manifest{}
+	for _, file := range zipFile.File {
+		file_name_lower := strings.ToLower(file.Name)
+		if !strings.HasPrefix(file_name_lower, pkg_path) || !file.Mode().IsRegular() || file_name_lower == version_manfiest_path {
+			continue
+		}
+		rc, _ := file.Open()
+		bytes, _ := io.ReadAll(rc)
+		manifests = append(manifests, Manifest{
+			FileName: strings.TrimPrefix(file.Name, fmt.Sprintf("%s-%s/", WINGET_PKGS_REPO_NAME, WINGET_PKGS_BRANCH)),
+			Content:  string(bytes),
+		})
+	}
+	return manifests
+}
+
+func downloadRepository(url, path string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("error getting response: %s", err)
+	}
+	out, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("error creating %s file: %s", path, err)
+	}
+	_, err = io.Copy(out, res.Body)
+	if err != nil {
+		return fmt.Errorf("error writing to %s file: %s", path, err)
+	}
+	return nil
+}
+
+func getPackagePath(pkg_id, version string, fileName ...string) string {
+	pkg_path := fmt.Sprintf("%s-%s/manifests/%s/%s", WINGET_PKGS_REPO_NAME, WINGET_PKGS_BRANCH, strings.ToLower(pkg_id[0:1]), strings.ReplaceAll(pkg_id, ".", "/"))
+	if len(version) > 0 {
+		pkg_path += "/" + version
+	}
+	if len(fileName) > 0 {
+		pkg_path += "/" + fileName[0]
+	}
+	return pkg_path
 }
 
 var common_fields_root_and_installer = []string{
