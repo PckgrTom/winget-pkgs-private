8509e8cb41bb5ab95439ac43ca1f7625cfe9ad86
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-02-26 11:28:51 +0000

    Update main.py (#231)
    * Update main.py
    
    * Update main.py

--- automation/main.py
+++ automation/main.py
@@ -86,6 +86,19 @@ def main() -> list[tuple[str, tuple[str, str, str]]]:
         "Authorization": f"Bearer {GH_TOKEN}"
     }]
 
+# Add HandBrake.HandBrake_Pckgr to Update List
+    id = "HandBrake.HandBrake_Pckgr"
+    JSON = requests.get("https://api.github.com/repos/HandBrake/HandBrake/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
+    Version = requests.get("https://api.github.com/repos/HandBrake/HandBrake/releases/latest", verify=False, headers=Headers[1]).json()["tag_name"]
+    Urls = [each["browser_download_url"] for each in JSON if ("exe" in each["browser_download_url"]) and not(("blockmap" in each["browser_download_url"]) or ("sig" in each["browser_download_url"])) and (("arm64" in each["browser_download_url"]) or ("x86_64" in each["browser_download_url"]))]
+    if not version_verify(Version, id):
+        report_existed(id, Version)
+    elif do_list(id, Version, "verify"):
+        report_existed(id, Version)
+    else:
+        Commands.append((command(Wingetcreate, list_to_str(Urls), Version, id, GH_TOKEN), (id, Version, "write")))
+    del JSON, Urls, Version, id
+
 # Add GitHub.Atom_Pckgr to Update List
     id = "GitHub.Atom_Pckgr"
     JSON = requests.get("https://api.github.com/repos/atom/atom/releases/latest", verify=False, headers=Headers[1]).json()["assets"]
